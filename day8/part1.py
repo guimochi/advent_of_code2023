8aac3743824c5a13909ef38b53f83c7349f1c427
guimochi <yamamoto.guilherme@outlook.com>
2023-12-08 11:02:32 +0100

    Day 8 - [x] Part 1 - [x] Part 2 Duration 2 hours Duration 45 minutes

--- day8/part1.py
+++ day8/part1.py
@@ -2,9 +2,78 @@ import time
 
 start_time = time.time()
 
-with open('input.txt', 'r') as f:
-    pass
+START_NODE = 'AAA'
+FINAL_NODE = 'ZZZ'
 
+class Noeud:
+    def __init__(self, value, left, right):
+        self.value = value
+        self.left = left
+        self.right = right
+
+    def give_next(self, char):
+        if char == 'L':
+            return self.left
+        if char == 'R':
+            return self.right
+
+
+def get_start(input, nodes, depart):
+    noeud = depart
+    for c in input:
+        noeud = nodes[noeud].give_next(c)
+    return noeud
+
+
+def get_path(start_node, nodes):
+    all_path = [['L', nodes[start_node].left], ['R', nodes[start_node].right]]
+    while True:
+        new_paths = []
+        for path in all_path:
+            path_left = path[0] + 'L'
+            node_left = nodes[path[1]].left
+            if node_left == 'ZZZ':
+                return path_left
+            new_paths.append([path_left, node_left])
+            path_right = path[0] + 'R'
+            node_right = nodes[path[1]].right
+            if node_right == 'ZZZ':
+                return path_right
+            new_paths.append([path_right, node_right])
+        all_path = new_paths
+    return None
+
+
+def get_length_path(input, start_node, nodes):
+    node = start_node
+    count = 0
+    while True:
+        for c in input:
+            node = nodes[node].give_next(c)
+            count += 1
+            if node == FINAL_NODE:
+                return count
+    return 0
+
+
+with open('input.txt', 'r') as f:
+    lines = f.readlines()
+    input = lines[0][:-1]
+    nodes = {}
+    # create nodes
+    for line in lines[2:]:
+        node = line.split(' = ')[0]
+        left, right = line.split(' = ')[1].strip('(').replace(')', "").strip().split(', ')
+        noeud = Noeud(node, left, right)
+        nodes[node] = noeud
+    # get start
+    start_node = get_start(input, nodes, 'AAA')
+    length = len(input)
+    if start_node == 'ZZZ':
+        print(length)
+        exit(0)
+    final_path = get_length_path(input, start_node, nodes)
+    print(length + final_path)
 end_time = time.time()
 elapsed_time = end_time - start_time
 print(f"The code took {elapsed_time} seconds to run.")
--- day8/part2.py
+++ day8/part2.py
@@ -2,8 +2,96 @@ import time
 
 start_time = time.time()
 
-with open('input.txt', 'r') as f:
+START_NODE_LETTER = 'A'
+FINAL_NODE_LETTER = 'Z'
+
+# Create noeud with left right attribute
+class Noeud:
+    def __init__(self, value, left, right):
+        self.value = value
+        self.left = left
+        self.right = right
+
+
+    # Gives the corresponding node depending on the letter provided
+    def give_next(self, char):
+        if char == 'L':
+            return self.left
+        if char == 'R':
+            return self.right
+
+def pgcd(a, b):
+    while b != 0:
+        a, b = b, a % b
+    return a
+
+def ppcm(a, b):
+    return a * b // pgcd(a, b)
+
+def ppcm_liste(nombres):
+    resultat = nombres[0]
+    for nombre in nombres[1:]:
+        resultat = ppcm(resultat, nombre)
+    return resultat
+
+# return the number of chr in input used and the last node it was on
+def get_length_path(input, start_nodes, map_node_noeud):
+    current_nodes = start_nodes
+    count = 0
+    i = 0
+    while True:
+        for c in input:
+            reached = 0
+            new_nodes = []
+            for node in current_nodes:
+                new_node = map_node_noeud[node].give_next(c)
+                if new_node[2] == FINAL_NODE_LETTER:
+                    reached += 1
+                new_nodes.append(new_node)
+            count += 1
+            if reached == 5 or reached == 6:
+                print(reached)
+            if reached == len(new_nodes):
+                return count, new_node
+            current_nodes = new_nodes
+
+    return 0
+
+# in the code, I will refer the class as Noeud and the 3 letter as node
+def get_cycle(input, node, map_node_noeud):
+    first_find, node = get_length_path(input, [node], map_node_noeud)
+    cycle_len = get_length_path(input, [node], map_node_noeud)
+    return first_find
+    # return first_find, cycle_len[0]
     pass
+
+
+def get_cycles(input, start_nodes, map_node_noeud):
+    return [get_cycle(input,node, map_node_noeud) for node in start_nodes]
+
+
+with open('input.txt', 'r') as f:
+    lines = f.readlines()
+    input = lines[0][:-1]
+    map_node_noeud = {}
+    start_nodes = []
+    # create map and get all starting nodes
+    for line in lines[2:]:
+        node = line.split(' = ')[0]
+        left, right = line.split(' = ')[1].strip('(').replace(')', "").strip().split(', ')
+        noeud = Noeud(node, left, right)
+        map_node_noeud[node] = noeud
+        if node[2] == START_NODE_LETTER:
+            start_nodes.append(node)
+
+
+    cycles = get_cycles(input, start_nodes, map_node_noeud)
+
+    print(ppcm_liste(cycles))
+
+
+    # final_path = get_length_path(input, start_nodes, map_node_noeud)
+    # print(final_path)
 
 end_time = time.time()
 elapsed_time = end_time - start_time
