5b7a81468cddf388b99c96c8f185ae877f8614ef
guimochi <yamamoto.guilherme@outlook.com>
2023-12-09 07:08:52 +0100

    Day 9 - [x] Part 1 - [x] Part 2 Duration 1 hour Duration 7 minutes

--- day9/part1.py
+++ day9/part1.py
@@ -2,8 +2,35 @@ import time
 
 start_time = time.time()
 
+# get last from list
+def last(liste):
+    return len(liste) - 1
+
+
 with open('input.txt', 'r') as f:
-    pass
+    total = 0
+    for line in f.readlines():
+        # get inputs
+        nums = [(list(map(int, line.split())))]
+        j = 0
+        # create the serie of nb
+        while (sum(nums[len(nums) - 1]) != 0):
+            new_serie = []
+            for i in range(1, len(nums[j])):
+                new_serie.append(nums[j][i] - nums[j][i - 1])
+            nums.append(new_serie)
+            j += 1
+
+        # add lat numbers
+        nums[last(nums)].append(0)
+        for i in range(last(nums), 0, -1):
+            i_last = last(nums[i])
+            current_num = nums[i][i_last]
+            last_num = nums[i - 1][i_last]
+            nums[i - 1].append(current_num + last_num)
+
+        total += nums[0][last(nums[0])]
+    print(total)
 
 end_time = time.time()
 elapsed_time = end_time - start_time
--- day9/part2.py
+++ day9/part2.py
@@ -2,8 +2,35 @@ import time
 
 start_time = time.time()
 
+# get last from list
+def last(liste):
+    return len(liste) - 1
+
+
 with open('input.txt', 'r') as f:
-    pass
+    total = 0
+    for line in f.readlines():
+        # get inputs
+        nums = [(list(map(int, line.split())))]
+        j = 0
+        # create the serie of nb
+        while (sum(nums[len(nums) - 1]) != 0):
+            new_serie = []
+            for i in range(1, len(nums[j])):
+                new_serie.append(nums[j][i] - nums[j][i - 1])
+            nums.append(new_serie)
+            j += 1
+
+        # add lat numbers
+        nums[last(nums)].insert(0, 0)
+        for i in range(last(nums), 0, -1):
+            i_first = 0
+            current_num = nums[i][i_first]
+            last_num = nums[i - 1][i_first]
+            nums[i - 1].insert(i_first, last_num - current_num)
+        print(nums)
+        total += nums[0][0]
+    print(total)
 
 end_time = time.time()
 elapsed_time = end_time - start_time
