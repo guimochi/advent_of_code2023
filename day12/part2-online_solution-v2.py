f5b57c3cb877c7e09d8d37984fd948a3f12b1066
guimochi <yamamoto.guilherme@outlook.com>
2023-12-12 11:01:05 +0100

    Day 12 - [x] Part 1 - [ ] Part 2 Duration 1 hours 40 minutes Checked another solution for part 2

---
+++ day12/part2-online_solution-v2.py
@@ -0,0 +1,47 @@
+cache = {}
+
+
+def count(cfg, nums):
+    # check si on est dans le cache, la fonction ne sera lance que si on n'est pas dedans
+    key = (cfg, nums)
+    if key in cache:
+        return cache[key]
+
+    if cfg == "":
+        return 1 if not nums else 0
+    if not nums:
+        return 0 if "#" in cfg else 1
+
+    results = 0
+    # dans le cas ou c'est . ou ? on va continuer a parcourir ou le consider comme un .
+    if cfg[0] in ".?":
+        results += count(cfg[1:], nums)
+    # dans le cas ou c'est un # ou ?. on va faire le check pour voir s'il est possible ce continuer
+    if cfg[0] in "#?":
+        if (
+            # s'il faut plus de block qu'il n'y de d'espace
+            nums[0] <= len(cfg)
+            # s'il y a un point par les nums[0] prochain caractere
+            and "." not in cfg[: nums[0]]
+            # si le caractere apres le block est vide ou n'est pas un #
+            and (len(cfg) == nums[0] or cfg[nums[0]] != "#")
+        ):
+            # on continue le scenario en passant au caractere suivant
+            results += count(cfg[nums[0] + 1 :], nums[1:])
+
+    # enregistre le resultat dans le cache
+    cache[key] = results
+    return results
+
+
+with open("input.txt", "r") as f:
+    total = 0
+    for line in f.readlines():
+        cfg, nums = line.split()
+        nums = tuple(map(int, nums.split(",")))
+
+        cfg = "?".join([cfg] * 5)
+        nums *= 5
+
+        total += count(cfg, nums)
+    print(total)
