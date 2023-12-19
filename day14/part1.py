14faa02600581945566ae579d28fa26cea763f06
guimochi <yamamoto.guilherme@outlook.com>
2023-12-19 11:45:19 +0100

    Day 19 - [x] Part 1 - [ ] Part 2 Duration 1 hour 30 minutes

--- day14/part1.py
+++ day14/part1.py
@@ -2,8 +2,24 @@ import time
 
 start_time = time.time()
 
-with open('input.txt', 'r') as f:
-    pass
+with open("test.txt", "r") as f:
+    grid = [line for line in f.read().split("\n")]
+
+    len_grid = len(grid)
+
+    total_weight = 0
+    # check col by col
+    for c, chs in enumerate(zip(*grid)):
+        # last obstacle seen
+        last_obstacle = -1
+        for r, ch in enumerate(chs):
+            if ch == "#":
+                last_obstacle = r
+            if ch == "O":
+                last_obstacle += 1
+                total_weight += len_grid - last_obstacle
+    print(total_weight)
+
 
 end_time = time.time()
 elapsed_time = end_time - start_time
--- day14/part2.py
+++ day14/part2.py
@@ -2,8 +2,50 @@ import time
 
 start_time = time.time()
 
-with open('input.txt', 'r') as f:
-    pass
+with open("test.txt", "r") as f:
+    grid = [line for line in f.read().split("\n")]
+
+    r_obj = {}
+    c_obj = {}
+
+    for r, row in enumerate(grid):
+        for c, ch in enumerate(row):
+            if ch == ".":
+                continue
+            if not r_obj.get(r):
+                r_obj[r] = [[], []]
+            r_obj[r][ch == "O"].append(c)
+            if not c_obj.get(c):
+                c_obj[c] = [[], []]
+            c_obj[c][ch == "O"].append(r)
+
+    # to move north
+    for c, (rocks, balls) in c_obj.items():
+        last_place = 0
+        place = []
+        for rock in rocks:
+            place.extend(
+                [
+                    i
+                    for i in range(
+                        last_place,
+                        last_place
+                        + len([ball for ball in balls if last_place <= ball < rock]),
+                    )
+                ]
+            )
+            if len(place) == len(balls):
+                break
+            last_place = rock + 1
+        place.extend(
+            [i for i in range(last_place, last_place + len(balls) - len(place))]
+        )
+        c_obj[c][1] = place
+
+    print(c_obj)
+    len_grid = len(grid)
+    total = sum(len_grid - ball for rocks, balls in c_obj.values() for ball in balls)
+    print(total)
 
 end_time = time.time()
 elapsed_time = end_time - start_time
